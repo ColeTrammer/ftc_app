eb6a815d0a64a1672700641a8a74e808470a24a1
ColeTrammer <cole.trammer@gmail.com>
2018-12-07 20:05:28 -0800

    Things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cole/TeleOp11285.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cole/TeleOp11285.java
@@ -13,7 +13,7 @@ public class TeleOp11285 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         XOmniDrive drive = new XOmniDrive(hardwareMap);
-        //DcMotor lift = hardwareMap.get(DcMotor.class, "lift");
+        DcMotor lift = hardwareMap.get(DcMotor.class, "lift");
 
         //telemetry.addData("Lift Encoder", lift.getCurrentPosition());
         //telemetry.update()
@@ -23,11 +23,11 @@ public class TeleOp11285 extends LinearOpMode {
         while (opModeIsActive()) {
             drive.run(gamepad1, gamepad2);
             if (gamepad1.a) {
-            //    lift.setPower(1);
+                lift.setPower(1);
             } else if (gamepad1.b) {
-            //    lift.setPower(-1);
+                lift.setPower(-1);
             } else {
-            //    lift.setPower(0);
+                lift.setPower(0);
             }
         }
     }
